873b0ee4b336113803bc7e126fd7607f3c4273a3
Yujeong <87570758+uuusssseerrr@users.noreply.github.com>
2026-04-16 18:56:30 +0900

    Create tb_tick_gen_100hz.v

---
+++ tb_tick_gen_100hz.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+
+module tb_tick_gen_100hz();
+
+    reg clk, rst;
+    wire o_tick_100hz;
+
+
+tick_gen_100hz dut (
+    .clk(clk),
+    .rst(rst),
+    .o_tick_100hz(o_tick_100hz)
+);
+
+    always #5 clk = ~ clk;
+
+    initial begin
+        clk=0;
+        rst=1;
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+
+        rst=0;
+
+        repeat (6_100_000_000) @(negedge clk);
+        $stop;
+    end
+
+endmodule
